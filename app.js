ecc797a500e9ec3c49217e128761db0318b4c490
sajay <zajubabu7@gmail.com>
2026-04-24 17:29:03 +0530

    mongoDB connection added

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+require("dotenv").config();
+const connectDB = require("./config/db");
 const express = require("express");
 const app = express();
 
+connectDB();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("iZen E-commerce running");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, ()  => {
     console.log("Server running on port 3000");
 });
